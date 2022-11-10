da5f31204b19c659ba055fe3a52b5742fa1daa51
airondevt <airondev@gmail.com>
2022-11-10 16:08:49 +0100

    clean code template without container

--- src/infra/logger/access_logs.ts
+++ src/infra/logger/access_logs.ts
@@ -1,16 +1,15 @@
 import morgan from 'morgan';
-// import path from 'path';
-// import * as rfs from 'rotating-file-stream'; // version 2.x
+import path from 'path';
+import * as rfs from 'rotating-file-stream'; // version 2.x
 
 export const ServerLogs = app => {
   // create a rotating write stream
-
-  // const accessLogStream = rfs.createStream('access.log', {
-  //   interval: '1d', // rotate daily
-  //   path: path.join(__dirname, 'log'),
-  // });
+  const accessLogStream = rfs.createStream('access.log', {
+    interval: '1d', // rotate daily
+    path: path.join(__dirname, '../../../public/logs'),
+  });
 
   // setup the logger
-  // app.use(morgan('combined', {stream: accessLogStream}));
+  app.use(morgan('combined', {stream: accessLogStream}));
   app.use(morgan('combined'));
 };
